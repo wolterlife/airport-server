67bad30e41c404ee94e4eceac440dcac16cc2917
Denis Wolter <wolter.important@gmail.com>
2022-10-09 01:21:52 +0300

    add empty controllers

---
+++ controllers/FlightsController.js
@@ -0,0 +1,7 @@
+exports.addFlight = function (req, res) {
+  res.json('add flight command');
+}
+
+exports.getFlight = function (req, res) {
+  res.json('get all flights');
+}
--- router.js
+++ router.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const flightController = require('./controllers/FlightsController')
 
-router.get("/", (req, res) => {
-  res.json("Banan");
-})
+router.get('/', (req, res) => {
+  res.json("home get");
+});
+
+router.get("/flights", flightController.getFlight);
+router.post("/flights", flightController.addFlight);
 
 module.exports = router;
