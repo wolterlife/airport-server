b936ff7ee2d5467899015f6f4d774fec962167c0
Denis Wolter <wolter.important@gmail.com>
2022-10-21 12:03:10 +0300

    add two options in getBy TicketController.ts

--- src/controllers/TicketController.ts
+++ src/controllers/TicketController.ts
@@ -13,6 +13,37 @@ exports.getAllTickets = async function (req: Request, res: Response) {
     res.json(tickets);
 }
 
+exports.getBy = async function (req: Request, res: Response) {
+    let ticket;
+    switch (req.query.method) {
+        case "by-user": // Поиск по пассажиру
+            ticket = await AppDataSource
+                .getRepository(Ticket)
+                .createQueryBuilder("ticket")
+                .where("ticket.login = :login", {login: req.params.id})
+                .leftJoinAndSelect("ticket.login", "login")
+                .leftJoinAndSelect("ticket.flight", "flight")
+                .getMany()
+            break;
+        case "by-flight": // Поиск по рейсу
+            ticket = await AppDataSource
+                .getRepository(Ticket)
+                .createQueryBuilder("ticket")
+                .where("ticket.flight = :flight", {flight: +req.params.id})
+                .leftJoinAndSelect("ticket.login", "login")
+                .leftJoinAndSelect("ticket.flight", "flight")
+                .getMany()
+            break;
+        default: {
+            res.status(400).json({msg: "Ошибка query параметров"})
+            return
+        }
+    }
+    if (ticket) res.json(ticket)
+    else res.sendStatus(404);
+}
+
+/*
 exports.getByLogin = async function (req: Request, res: Response) {
     const ticket = await AppDataSource
         .getRepository(Ticket)
@@ -38,6 +69,7 @@ exports.getByFlight = async function (req: Request, res: Response) {
         res.json(ticket);
     } else res.sendStatus(404);
 }
+* */
 
 exports.createTicket = async function (req: Request, res: Response) {
     const ticket = new Ticket();
